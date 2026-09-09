6765c2b13227ed964f1ca2b0bbcf5d7735114d25
vni5104 <vickyn5104@gmail.com>
2026-09-09 14:49:04 -0400

    in class demo 9/9

--- src/responses.js
+++ src/responses.js
@@ -12,6 +12,12 @@ const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
 };
 
+const getCats = (request, response) => {
+  const cat = { name: 'Captain Peanut-Butter', age: 7 };
+
+  respond(request, response, JSON.stringify(cat), 'application/json');
+};
+
 module.exports = {
   getCats,
   getIndex,
--- src/server.js
+++ src/server.js
@@ -4,13 +4,25 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-
+  '/': responseHandler.getIndex,
+  '/cats': responseHandler.getCats,
+  default: responseHandler.getIndex
 };
 
 const onRequest = (request, response) => {
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedURL = new URL(request.url, `${protocol}://${request.headers.host}`);
+  console.log(parsedURL);
+  
+  const handler = urlStruct[parsedURL.pathname];
 
+  if (handler) {
+    handler(request, response);
+  } else {
+    urlStruct.default(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on 127.0.0.1: ${port}`);
+  console.log(`Listening on 127.0.0.1:${port}`);
 });
